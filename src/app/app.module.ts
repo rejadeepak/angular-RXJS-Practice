7838dc5366a632124da425369d8ef6a9ab427517
rejadeepak <deepak.reja@gmail.com>
2020-01-28 11:03:03 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router'
 
 
 
@@ -11,6 +12,7 @@ import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
@@ -18,12 +20,19 @@ import { ProductDetailComponent } from './products/product-detail.component';
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-   HttpClientModule
+   HttpClientModule,
+   RouterModule.forRoot([
+     { path:'products',component:ProductListComponent},
+     { path:'product/:id',component:ProductDetailComponent },
+     { path:'welcome',component:WelcomeComponent}
+
+   ])
   ],
   bootstrap: [AppComponent]
 })
